10548a7b933b83bfe46d9e454dd9d3f0be6299f8
Sakib Hasan <109980546+sakibhasan017@users.noreply.github.com>
2024-04-19 11:41:26 +0600

    Add files via upload

---
+++ CircularLinkedList.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+using namespace std;
+const int M=1e9+7;
+const int N=2e5+7;
+
+struct Node{
+
+ Node *previous,*next;
+ int data;
+};
+
+int main() {
+   Node *head=new Node;
+   Node *tail=new Node;
+   head->previous=NULL;
+   Node *Hsave=head;
+   Hsave->previous=NULL;
+   int n;
+   cin>>n;
+   for(int i=0;i<n;i++){
+     int x;
+     cin>>x;
+     Node *newNode=new Node;
+     newNode->data=x;
+     newNode->previous=Hsave;
+     newNode->next=NULL;
+     Hsave->next=newNode;
+     Hsave=newNode;
+
+   }
+   
+   Hsave->next=head;
+   
+   Node *i;
+   int w=6;
+   while(w--){
+   for(i=head->next;i!=head;i=i->next){
+      cout<<i->data<<" ";
+   }
+   cout<<i->next->data<<endl;
+   cout<<endl;
+   }
+
+}
