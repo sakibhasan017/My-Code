03b335e346d9e442f41b81909e1831d2bc7e6aa4
Sakib Hasan <109980546+sakibhasan017@users.noreply.github.com>
2024-05-30 08:56:14 +0600

    DFSCYCLE.cpp

--- DFSCYCLE.cpp
+++ DFSCYCLE.cpp
@@ -53,3 +53,58 @@ int main(){
     }
     return 0;
 }
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+const int M = 10004;
+
+vector<int> adj[M];
+bool vis[M];
+
+bool dfs(int u, int parent) {
+    vis[u] = true;
+    for (int v : adj[u]) {
+        if (!vis[v]) {
+            if (dfs(v, u)) return true;
+        } else if (v != parent) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    memset(vis, false, sizeof(vis));
+
+    int n;
+    cout << "Enter number of edges: ";
+    cin >> n;
+
+    cout << "Enter edges (u v):\n";
+    for (int i = 0; i < n; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    bool hasCycle = false;
+
+    for (int i = 1; i < M; i++) {
+        if (!vis[i] && !adj[i].empty()) {
+            if (dfs(i, -1)) {
+                hasCycle = true;
+                break;
+            }
+        }
+    }
+
+    if (hasCycle)
+        cout << "There is a Cycle" << endl;
+    else
+        cout << "There is no Cycle" << endl;
+
+    return 0;
+}
+*/
